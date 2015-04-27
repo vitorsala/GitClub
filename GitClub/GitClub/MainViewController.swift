0ca4b9ab2b5a4c22266456d1ed0b67f1436e1b78
JoaoMarcos <jmra10@gmail.com>
2015-04-27 16:00:58 -0300

    popup

--- GitClub/GitClub/MainViewController.swift
+++ GitClub/GitClub/MainViewController.swift
@@ -12,14 +12,32 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var user: UITextField?
+        
+        var alertController = UIAlertController(title: "Teste", message: "Oi Tudo bem?", preferredStyle: .Alert)
+        
+        let buttonOk: UIAlertAction = UIAlertAction(title: "OK", style: .Default) { (UIAlertAction) -> Void in
+            println("User: \(user?.text)")
+        }
+        
+        let buttonCancel: UIAlertAction = UIAlertAction(title: "Cancel", style: .Cancel) { (UIAlertAction) -> Void in}
+        
+        alertController.addAction(buttonOk)
+        alertController.addAction(buttonCancel)
+        
+        alertController.addTextFieldWithConfigurationHandler { (textField: UITextField!) -> Void in
+            textField.placeholder = "User"
+            user = textField
+        }
+        
+        self.presentViewController (alertController, animated: true, completion: nil)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
